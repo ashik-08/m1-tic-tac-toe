29b86067b77b0356e448a5f6e4cc9923e62d6469
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-07 05:33:03 +0600

    [refactor](App): add square button using React reusable component
     - add useState hook to handle value state
    
     - add onClick to the button to show value

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold"
+    >
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
   return (
     <>
-      <div>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <div>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <div>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
-        <button className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold">
-          X
-        </button>
+      <div className="flex">
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
