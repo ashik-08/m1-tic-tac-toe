2aa75d8badaadef2470b4588310c654a5324f898
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-07 21:41:35 +0600

    [feat](App): add history list button to traverse through previous moves

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const [gameOver, setGameOver] = useState(false);
-
+function Board({ xIsNext, squares, gameOver, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares) || gameOver) {
       return;
@@ -23,11 +19,7 @@ export default function Board() {
 
     const nextSquares = squares.slice();
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-    if (!nextSquares.includes(null)) {
-      setGameOver(true);
-    }
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -59,6 +51,64 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [gameOver, setGameOver] = useState(false);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setGameOver(!nextSquares.includes(null) && true);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+    setGameOver(history[move].includes(null) ? false : true);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move === 0) {
+      description = "Go to game start";
+    } else {
+      description = `Go to move #${move}`;
+    }
+    return (
+      <li key={move}>
+        <button
+          onClick={() => jumpTo(move)}
+          className="bg-slate-200 font-medium border border-blue-300 px-2 py-1 w-full rounded"
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <section className="flex flex-wrap py-8 justify-center items-center gap-8 min-h-screen">
+      <div>
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          gameOver={gameOver}
+          onPlay={handlePlay}
+        />
+      </div>
+      <div>
+        <ol className="space-y-2 border p-3">{moves}</ol>
+      </div>
+    </section>
   );
 }
 
