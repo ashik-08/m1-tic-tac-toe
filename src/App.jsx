8a62de12d6595193b4a41b28fb520107eb6d934a
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-07 14:56:57 +0600

    [feat](App): add winner of the game
     - add calculateWinner function to set the winner
    
     - add status to show winner otherwise player turn

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Square({ value, onSquareClick }) {
   return (
     <button
       onClick={onSquareClick}
-      className="bg-white border border-gray-400 w-12 h-12 m-1 rounded-md text-lg font-semibold"
+      className="bg-white border border-gray-400 w-16 h-16 m-px rounded text-3xl font-bold"
     >
       {value}
     </button>
@@ -16,7 +16,7 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -26,8 +26,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Now playing: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <h1 className="text-2xl font-medium italic my-4">{status}</h1>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -45,4 +54,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
