e75b299693906b229f73b04864b767a6e6ac827d
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-07 14:10:23 +0600

    [update](App): add toggle value to play for two
     - add isNext state to remember the player turn
    
     - toggled next value so after one another can put their symbol
    
     - can put one value only inside one square

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,19 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
+
   return (
     <>
       <div className="flex">
